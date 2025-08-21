adb0ab9ca1fe94d29a4600702b562a0042ebf38b
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-08-21 18:11:30 +0600

    dommm

--- js/cssInJS.js
+++ js/cssInJS.js
@@ -1,1 +1,18 @@
 console.log("file conected");
+const sections = document.querySelectorAll("section");
+// for (const section of sections) {
+//   //   section.style.backgroundColor = "purple";
+//   section.style.border = "2px solid blue";
+//   section.style.marginBottom = "8px";
+//   section.style.borderRadius = "10px";
+//   section.style.padding = "20px";
+// }
+const some = 23;
+let some1 = 40;
+
+for (const section of sections) {
+  section.classList.add("section-card");
+}
+
+// const section = document.querySelector("section");
+// section.style.backgroundColor = "purple";
