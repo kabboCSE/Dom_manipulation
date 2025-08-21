b8258cb323d270a0dbe45f431282876d89d1b3d5
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-08-21 08:23:54 +0600

    dom

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-console.log("Hey There!!");
-console.log(document);
+// console.log("Hey There!!");
+// console.log(document);
 const student = {
   name: "kabbo",
   id: 21,
   study: function (sub) {
-    console.log("studing sub", sub);
+    // console.log("studing sub", sub);
   },
 };
 
 const heading = document.getElementsByTagName("h1");
-console.log(heading);
+// console.log(heading);
 for (const head of heading) {
-  console.log(head.innerText);
+  // console.log(head.innerText);
 }
